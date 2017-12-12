c5651b564132a9f98a554f859b9718ca9889d615
shrutisankolli <shruti3@illinois.edu>
2017-12-11 21:28:38 -0600

    Adding plot to the distribution

--- UserInterface.py
+++ UserInterface.py
@@ -1,5 +1,6 @@
 from StockAnalysisServer import StockAnalysisServer
 import datetime
+import matplotlib.pyplot as plt
 class UserInterface:
     if __name__ == '__main__':
         ticker = input("Enter the name of the stock")
@@ -7,5 +8,7 @@ class UserInterface:
         base_amount = input("Enter the starting amount you wish to invest:")
         finalAmount = input("Enter the final amount that you wish to see after " + str(time_horizon) + " :")
         StockAnalysisServer = StockAnalysisServer()
-        probablity = StockAnalysisServer.get_expected_stock_return_probablity(ticker,time_horizon,base_amount,finalAmount)
+        dist,probablity = StockAnalysisServer.get_expected_stock_return_probablity(ticker,time_horizon,base_amount,finalAmount)
         print(probablity)
+        plt.hist(dist)
+        plt.show()
